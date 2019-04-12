31f40002fb4230f5145677989bf12580c49d5e80
Jiang <505514870@qq.com>
2019-04-12 14:53:03 +0800

    2019.04.12

---
+++ 200. Number of Islands/index.js
@@ -0,0 +1,56 @@
+/**
+ * 200. Number of Islands
+ * 
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+ * You may assume all four edges of the grid are all surrounded by water.
+ * 
+ * Example 1:
+ *  Input:
+ *  11110
+ *  11010
+ *  11000
+ *  00000
+ *  Output: 1
+ * 
+ * Example 2:
+ *  Input:
+ *  11000
+ *  11000
+ *  00100
+ *  00011
+ *  Output: 3
+ */
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (grid) {
+
+  let count = 0
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === '1') {
+        count++
+        dfs(i, j)
+      }
+    }
+  }
+
+  function dfs(i, j) {
+    if (i < 0 || i === grid.length || j < 0 || j === grid[i].length) return
+
+    if (grid[i][j] === '0') return
+
+    grid[i][j] = '0'
+
+    dfs(i, j + 1)
+    dfs(i + 1, j)
+    dfs(i - 1, j)
+    dfs(i, j - 1)
+  }
+
+
+  return count
+};
