65b3ec12741e99a1d4390f6951d672815099aaab
Jiang <505514870@qq.com>
2019-04-18 15:53:55 +0800

    2019.04.18

---
+++ 236. Lowest Common Ancestor of a Binary Tree/index.js
@@ -0,0 +1,35 @@
+/**
+ * 236. Lowest Common Ancestor of a Binary Tree
+ * 
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+
+  if (!root) return null
+  if (root === p || root === q) {
+    return root
+  }
+
+  let left = lowestCommonAncestor(root.left, p, q)
+  let right = lowestCommonAncestor(root.right, p, q)
+
+  if (left && right) {
+    return root
+  }
+  if (left) return left
+  if (right) return right
+};
