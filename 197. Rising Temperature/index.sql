68781f593abcda6dc6054e5899780fbad236b0eb
Jiang <505514870@qq.com>
2019-08-14 10:22:34 +0800

    2019.08.14

---
+++ 197. Rising Temperature/index.sql
@@ -0,0 +1,30 @@
+-- 197. Rising Temperature
+-- Create table If Not Exists Weather (Id int, RecordDate date, Temperature int)
+-- Truncate table Weather
+-- insert into Weather (Id, RecordDate, Temperature) values ('1', '2015-01-01', '10')
+-- insert into Weather (Id, RecordDate, Temperature) values ('2', '2015-01-02', '25')
+-- insert into Weather (Id, RecordDate, Temperature) values ('3', '2015-01-03', '20')
+-- insert into Weather (Id, RecordDate, Temperature) values ('4', '2015-01-04', '30')
+
+-- Given a Weather table, write a SQL query to find all dates' Ids with higher temperature compared to its previous (yesterday's) dates.
+
+-- +---------+------------------+------------------+
+-- | Id(INT) | RecordDate(DATE) | Temperature(INT) |
+-- +---------+------------------+------------------+
+-- |       1 |       2015-01-01 |               10 |
+-- |       2 |       2015-01-02 |               25 |
+-- |       3 |       2015-01-03 |               20 |
+-- |       4 |       2015-01-04 |               30 |
+-- +---------+------------------+------------------+
+-- For example, return the following Ids for the above Weather table:
+
+-- +----+
+-- | Id |
+-- +----+
+-- |  2 |
+-- |  4 |
+-- +----+
+
+-- # Write your MySQL query statement below
+select weather.Id as 'Id'
+from weather join weather w on datediff(weather.RecordDate, w.RecordDate) = 1 and weather.Temperature > w.Temperature;
