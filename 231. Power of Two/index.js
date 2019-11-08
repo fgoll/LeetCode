19cb44e9911b522fd8fe30a05c6b0ec6f7349b78
Jiang <505514870@qq.com>
2019-11-08 09:19:48 +0800

    2019.11.08

---
+++ 231. Power of Two/index.js
@@ -0,0 +1,13 @@
+/**
+ * 231. Power of Two
+ * 
+ * https://leetcode.com/problems/power-of-two/
+ */
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+  return n > 0 && ((n & (n - 1)) === 0)
+};
