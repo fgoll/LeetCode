e99551ab1c35dd211aa2d55ac228cf91d78f0666
Jiang <505514870@qq.com>
2020-02-28 09:19:48 +0800

    2020.02.28

---
+++ 342. Power of Four/index.js
@@ -0,0 +1,23 @@
+/**
+ * 342. Power of Four
+ * 
+ * Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+ * 
+ * Example 1:
+ *  Input: 16
+ *  Output: true
+ * 
+ * Example 2:
+ *  Input: 5
+ *  Output: false
+ * 
+ * Follow up: Could you solve it without loops/recursion?
+ */
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPowerOfFour = function(num) {
+  return (num > 0 && ((num & (num-1)) === 0) && ((num & 0x55555555) === num))  
+};
