e007724612f05ab8fcf6c3f8e9bb27e358185181
Jiang <505514870@qq.com>
2020-03-13 20:28:05 +0800

    2020.03.13

---
+++ 363. Max Sum of Rectangle No Larger Than K/index.js
@@ -0,0 +1,55 @@
+/**
+ * 363. Max Sum of Rectangle No Larger Than K
+ * 
+ * Given a non-empty 2D matrix matrix and an integer k, 
+ * find the max sum of a rectangle in the matrix such that its sum is no larger than k.
+ * 
+ * Example:
+ *  Input: matrix = [[1,0,1],[0,-2,3]], k = 2
+ *  Output: 2 
+ *  Explanation: Because the sum of rectangle [[0, 1], [-2, 3]] is 2,
+ *               and 2 is the max number no larger than k (k = 2).
+ * 
+ * Note:
+ *  The rectangle inside the matrix must have an area > 0.
+ *  What if the number of rows is much larger than the number of columns?
+ */
+
+/**
+ *  [[1,0,1],
+ *   [0,-2,3]],
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var maxSumSubmatrix = function(matrix, k) {
+  let row = matrix.length
+  let col = matrix[0].length
+  let max = Number.MIN_SAFE_INTEGER
+  for (let left = 0; left < col; left ++) {
+    let sums = [...Array(row)].map(_ => 0)
+
+    for (let right = left; right < col; right ++) {
+
+      for (let i = 0; i < row; i ++) {
+        sums[i] += matrix[i][right]
+      }
+
+      let maxSum = Number.MIN_SAFE_INTEGER
+      for (let i = 0; i < sums.length; i ++) {
+        let curSum = 0
+        for (let j = i; j < sums.length; j ++) {
+          curSum += sums[j]
+          if(curSum == k) return k;
+
+          if (curSum > maxSum && curSum <= k) {
+            maxSum = curSum
+          }
+        }
+      }
+      
+      max = Math.max(max, maxSum)
+    }
+  }
+  return max
+};
