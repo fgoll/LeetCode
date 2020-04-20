22fd826c0daafbd1439e9c68b5b26e473d922014
Jiang <505514870@qq.com>
2020-04-20 17:27:26 +0800

    2020.04.20

---
+++ 371. Sum of Two Integers/index.js
@@ -0,0 +1,22 @@
+/**
+ * 371. Sum of Two Integers
+ * 
+ * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+ * 
+ * Example 1:
+ *  Input: a = 1, b = 2
+ *  Output: 3
+ * 
+ * Example 2:
+ *  Input: a = -2, b = 3
+ *  Output: 1
+ */
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+  return b==0? a:getSum(a^b, (a&b)<<1)
+};
